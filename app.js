1b3ae9a9c08e5d2adc948964f5b95ca8bac786f9
TheaNyashaKufa8910 <nyashakufa29@gmail.com>
2025-09-17 10:47:10 +0200

    upto tutorial 15

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ console.log(stuff.adder(stuff.pi,5));*/
 
 
 //Event emmitter//
-var events = require('events');
+/*var events = require('events');
 var util = require('util');
 
 var Person = function(name){
@@ -79,4 +79,71 @@ var Person = function(name){
    });
 
    james.emit('speak', 'hey dudes');
-   ryu.emit('speak', 'I want curry') 
+   ryu.emit('speak', 'I want curry');*/
+   
+   
+   //Reading and writing files//
+   var fs = require('fs');
+   //var readMe = fs.readFileSync('readMe.txt', 'utf8');
+   //console.log(readMe);//
+   //fs.writeFileSync('writeMe.txt', readMe);
+
+
+   /*fs.readFile('readMe.txt', 'utf8', function(err, data){
+    fs.writeFile('writeMe.txt', data);
+   });*/
+
+
+//creating/removing directories
+
+/*var fs = require('fs');
+
+fs.unlink('writeMe.txt')*/
+
+//creating a server//
+
+/*var http = require('http');
+var server = http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hey ninjas');
+});
+
+server.listen(3000, '127.0.0.1');
+console.log('yo dawgs, now listening to port 3000');*/
+
+// READABLE STREAMS//
+var http = require('http');
+var fs = require('fs');
+var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+
+myReadStream.on('data', function(chunk){
+    console.log('new chunk received:');
+    console.log(chunk);
+});
+
+
+/*var server = http.createServer(function(req, res){
+console.log('request was made: ' + req.url);
+res.writeHead(200, {'Content-Type': 'text/plain'});
+res.end('Hey, ninjas');
+}) 
+
+server.listen(3000, '127.0.0.1');
+console.log('yo dawgs, now listening to port 3000')
+
+*/
+
+
+//writtable streams//
+var http = require('http');
+var fs= require('fs');
+
+var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
+
+myReadStream.on('data', function(chunk){
+    console.log('new chunk received');
+    myWriteStream.write(chunk);
+});
+
+
